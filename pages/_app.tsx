7d8d3ec324b15f7f4470684e3207822cfe62b007
Akash Pattnaik <akashpattnaik.github@gmail.com>
2023-07-08 17:19:25 +0530

    Initial commit from create-refine-app

---
+++ pages/_app.tsx
@@ -0,0 +1,73 @@
+import { GitHubBanner, Refine } from "@refinedev/core";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import {
+  RefineSnackbarProvider,
+  ThemedLayoutV2,
+  notificationProvider,
+} from "@refinedev/mui";
+import routerProvider, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/nextjs-router";
+import type { NextPage } from "next";
+import { AppProps } from "next/app";
+
+import { Header } from "@components/header";
+import { ColorModeContextProvider } from "@contexts";
+import CssBaseline from "@mui/material/CssBaseline";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import dataProvider from "@refinedev/simple-rest";
+
+const API_URL = "https://api.fake-rest.refine.dev";
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  noLayout?: boolean;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout): JSX.Element {
+  const renderComponent = () => {
+    if (Component.noLayout) {
+      return <Component {...pageProps} />;
+    }
+
+    return (
+      <ThemedLayoutV2 Header={() => <Header sticky />}>
+        <Component {...pageProps} />
+      </ThemedLayoutV2>
+    );
+  };
+
+  return (
+    <>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <ColorModeContextProvider>
+          <CssBaseline />
+          <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+          <RefineSnackbarProvider>
+            <Refine
+              routerProvider={routerProvider}
+              dataProvider={dataProvider(API_URL)}
+              notificationProvider={notificationProvider}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+              }}
+            >
+              {renderComponent()}
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+          </RefineSnackbarProvider>
+        </ColorModeContextProvider>
+      </RefineKbarProvider>
+    </>
+  );
+}
+
+export default MyApp;
